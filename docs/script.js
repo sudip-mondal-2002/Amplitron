4e76f867a8c08c2b0dd70f62393658a378a31510
Vexx <advikdivekar@gmail.com>
2026-05-25 19:34:02 +0530

    Remove hardcoded PostHog API key from client-side source files

--- docs/script.js
+++ docs/script.js
@@ -58,7 +58,7 @@
         (e.__SV = 1))
 })(document, window.posthog || [])
 
-posthog.init("phc_ecTrgEi1Wfx1rgTVwSZxCWt9swjuWiKaXAYssHhHLNr", {
+posthog.init(window.POSTHOG_API_KEY || '', {
     api_host: "https://t.amplitron.sudipmondal.co.in",
     ui_host: "https://us.posthog.com",
     person_profiles: "identified_only",
