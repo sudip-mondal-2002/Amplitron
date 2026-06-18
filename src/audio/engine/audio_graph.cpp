56df9d29f5c40269a4c0e92bcb842b053fad86a3
codewithmudit <muditsingh.me@gmail.com>
2026-06-18 22:17:24 +0530

    Fix clang-format

--- src/audio/engine/audio_graph.cpp
+++ src/audio/engine/audio_graph.cpp
@@ -70,8 +70,9 @@ int AudioGraph::add_link(int source_pin_id, int dest_pin_id) {
                 }
             }
             if (out_count >= 1) {
-                printf("add_link failed: Output pin %d already has an outgoing connection!\n",
-                        source_pin_id);
+                printf(
+                    "add_link failed: Output pin %d already has an outgoing connection!\n",
+                    source_pin_id);
                 return -1;  // Each output pin can only have 1 outgoing connection!
             }
         }
