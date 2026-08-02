eb8c7a9473bc0530778ddb09e657b3500006018e
Bhavya Jain <jainism.bhavya0810@gmail.com>
2026-08-02 22:57:34 +0530

    refactor: simplify MIDI learn initialization guards

--- src/gui/views/gui_midi.cpp
+++ src/gui/views/gui_midi.cpp
@@ -224,11 +224,7 @@ bool GuiMidi::render_learn_menu_item(const std::string& effect_name,
         }
     }
 
-    if (ImGui::MenuItem("MIDI Learn")) {
-        if (!midi_.initialize()) {
-            return false;
-        }
-
+    if (ImGui::MenuItem("MIDI Learn") && midi_.initialize()) {
         midi_.start_learn(MidiTargetType::EffectParam, effect_name, param_name);
         return true;
     }
@@ -236,11 +232,7 @@ bool GuiMidi::render_learn_menu_item(const std::string& effect_name,
 }
 
 bool GuiMidi::render_learn_bypass_item(const std::string& effect_name) {
-    if (ImGui::MenuItem("MIDI Learn (Bypass)")) {
-        if (!midi_.initialize()) {
-            return false;
-        }
-
+    if (ImGui::MenuItem("MIDI Learn (Bypass)") && midi_.initialize()) {
         midi_.start_learn(MidiTargetType::EffectBypass, effect_name, "");
         return true;
     }
